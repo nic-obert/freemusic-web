2962bdc2fa1ad2083d80f052f2947c0fa7298c38
Nicholas Obert <nchlsuba@gmail.com>
2020-10-15 13:11:39 +0200

    files ready for heroku

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from frontend import api
+from frontend.api import app
 
 
 if __name__ == "__main__":
 
     # start web app
-    api.app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='127.0.0.1')
