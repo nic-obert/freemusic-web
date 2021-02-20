9354d383993bc4a7d188ab48c62dbc2cc89c405a
Nicholas Obert <nchlsuba@gmail.com>
2021-02-20 07:44:32 +0100

    chenged dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup, Extension
 
 setup(name='c_rename', version='1.0', \
-    ext_modules=[Extension('c_rename', ['rename.c'])])
+    ext_modules=[Extension('c_rename', ['c_extensions/rename.c'])])
